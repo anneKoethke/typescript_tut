c4081fe5cb65e6068356345c05b11def4c2027c2
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-22 10:47:24 +0200

    info on TS added

--- typescript_tutorial/sandbox.js
+++ typescript_tutorial/sandbox.js
@@ -1,9 +1,7 @@
 // compile in terminal: tsc sandbox.ts sandbos.js
 // if the TS and JS file are named the same or the JS doesn't exists, just use: tsc sandbox.ts
-// -> to automate the compile process: tsc file.ts -w
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// -> watching changes: tsc sandbox.ts -w
+// strict types in TS means you can't change a variables type later
+var character = 'mario'; // TS is infering what type this is (inference)
+var age = 35;
+var isBlackBelt = false;

23629291808a0f59c356ece9d2aee8436b35e2c6
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-22 10:22:48 +0200

    watching sandbox.ts

--- typescript_tutorial/sandbox.js
+++ typescript_tutorial/sandbox.js
@@ -1,6 +1,9 @@
 // compile in terminal: tsc sandbox.ts sandbos.js
 // if the TS and JS file are named the same or the JS doesn't exists, just use: tsc sandbox.ts
+// -> to automate the compile process: tsc file.ts -w
 var character = 'mario';
 console.log(character);
 var inputs = document.querySelectorAll('input');
-console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
--- typescript_tutorial/sandbox.ts
+++ typescript_tutorial/sandbox.ts
@@ -7,4 +7,6 @@ console.log(character);
 
 const inputs = document.querySelectorAll('input');
 
-console.log(inputs);
+inputs.forEach(input => {
+  console.log(input);
+});

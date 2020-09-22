8717a2d60ad00b17804d01ead627cf8da419ba7d
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-22 10:19:31 +0200

    first compiled JS

---
+++ typescript_tutorial/sandbox.js
@@ -0,0 +1,6 @@
+// compile in terminal: tsc sandbox.ts sandbos.js
+// if the TS and JS file are named the same or the JS doesn't exists, just use: tsc sandbox.ts
+var character = 'mario';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
--- typescript_tutorial/sandbox.ts
+++ typescript_tutorial/sandbox.ts
@@ -1,7 +1,10 @@
+// compile in terminal: tsc sandbox.ts sandbos.js
+// if the TS and JS file are named the same or the JS doesn't exists, just use: tsc sandbox.ts
+
 const character = 'mario';
 
 console.log(character);
 
-const inputs = document.querySeleczorAll('input');
+const inputs = document.querySelectorAll('input');
 
 console.log(inputs);

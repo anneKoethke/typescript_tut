57fd7be4eb98206c31fdbed93e665a99bee7d2e0
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-22 13:18:29 +0200

    first steps with DOM

--- typescript_tutorial/public/app.js
+++ typescript_tutorial/public/app.js
@@ -1,2 +1,6 @@
 "use strict";
-// 
+/* const anchor = document.querySelector('a');
+console.log(anchor.href); // error TS2531: Object is possibly 'null'. -> TS doesn't know the DOM beforehand, so could be null
+ */
+var anchor = document.querySelector('a'); // '!' tells TS, there is an anchor; error suppressed (you should be sure of this)
+console.log(anchor.href);
--- typescript_tutorial/src/app.ts
+++ typescript_tutorial/src/app.ts
@@ -1,1 +1,6 @@
-// 
+/* const anchor = document.querySelector('a');
+console.log(anchor.href); // error TS2531: Object is possibly 'null'. -> TS doesn't know the DOM beforehand, so could be null
+ */
+
+const anchor = document.querySelector('a')!; // '!' tells TS, there is an anchor; error suppressed (you should be sure of this)
+console.log(anchor.href);

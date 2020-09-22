fca33313bf5197feced0fa31b8b1f4fd86079f67
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-22 11:51:28 +0200

    added and configured tsconfig

---
+++ typescript_tutorial/public/sandbox.js
@@ -0,0 +1,5 @@
+"use strict";
+// get a tsconfig file to structure your compiling: tsc --init
+// -> set 'rootDir' for the TS files and 'outDir' for the JS files
+// -> terminal: tsc -w (instead of 'tsc root-path/TS-file.ts out-path/JS-file.js') 
+console.log('hello, world, again!');

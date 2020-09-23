3f09c1daca2035a8359fc80ec1d5982dbfed5011
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-23 10:27:46 +0200

    extracted mini project from tutorial

---
+++ Invoice_Payment_Project/public/app.js
@@ -0,0 +1,23 @@
+import { Invoice } from './classes/Invoice.js'; // IMPORTANT! MUST BE '.js', because that's what the browser sees!
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+// listTemplate instance
+const ul = document.querySelector('ul'); // using a class here will cause an error (still working, but looks bad)
+const list = new ListTemplate(ul);
+const form = document.querySelector('.new-item-form');
+// inputs 
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+});
---
+++ Invoice_Payment_Project/public/classes/ListTemplate.js
@@ -0,0 +1,21 @@
+export class ListTemplate {
+    constructor(container) {
+        this.container = container;
+    }
+    // item can be an Invoice or a Payment, thus use the Interface HasFormatter here
+    render(item, heading, pos) {
+        const li = document.createElement('li');
+        const h4 = document.createElement('h4');
+        h4.innerText = heading;
+        li.append(h4);
+        const p = document.createElement('p');
+        p.innerText = item.format(); // interface makes sure it has a format() method
+        li.append(p);
+        if (pos === 'start') {
+            this.container.prepend(li);
+        }
+        else {
+            this.container.append(li);
+        }
+    }
+}
